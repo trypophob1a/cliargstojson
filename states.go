8d95759aa4fc0f31513c44582f194ea31b76e260
trypophob1a <trypophob1a@mail.ru>
2024-03-24 03:45:16 +0500

    ci test

---
+++ states.go
@@ -0,0 +1,58 @@
+package cliargstojson
+
+const (
+	TokenTypeUnknown = iota
+	TokenTypeNumber
+	TokenTypeString
+	TokenTypeBoolean
+	TokenTypeArray
+	TokenTypeObject
+)
+
+/**
+  "person": {
+    "name": "John Doe-Hopkins",
+    "age": 25,
+    "phones": [
+      "+1 234 567 8901",
+      "+1 234 567 8902"
+    ],
+    "companies": [
+      {
+        "g2": {
+          "position": "developer",
+          "years": 5
+        }
+      },
+      {
+        "cow ball": {
+          "position": "manager",
+          "years": 2
+        }
+      }
+    ],
+    "isMarried": false,
+    "family": {
+      "mother": {
+        "name": "Jane Doe-Hopkins",
+        "age": 55
+      },
+      "father": {
+        "name": "Robert Doe-Hopkins",
+        "age": 57
+      }
+    }
+  }
+*/
+// -person. name="John Doe-Hopkins" age=30
+// phones=["+1 234 567 8901", "+1 234 567 8902"]
+// family. mother. name="Jane Doe-Hopkins" age=55
+// father. name="Robert Doe-Hopkins" age=57
+// -person. isMarried=false
+// companies=[g2. position="developer" years=5, cow ball. position="manager" years=2]
+
+const (
+	StateSearchKey = iota
+	StateParseKey
+	StateParseValue
+)
