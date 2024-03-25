2dcfa736150877917938b7d1eec18801bdc62a32
trypophob1a <trypophob1a@yandex.ru>
2024-03-25 10:02:47 +0000

    go sum 1

--- cliargstojson.go
+++ cliargstojson.go
@@ -1,1 +1,15 @@
 package cliargstojson
+
+import (
+	"log/slog"
+	"strconv"
+)
+
+func Sum(a, b int) int {
+	if a < 0 || b < 0 {
+		aStr := strconv.Itoa(a)
+		bStr := strconv.Itoa(b)
+		slog.Info("a = %v and b = %v must be non-negative", aStr, bStr)
+	}
+	return a + b
+}
