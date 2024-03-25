6bfd0ec09851205fd8413221a3259c9d80fa2ac0
trypophob1a <trypophob1a@mail.ru>
2024-03-26 03:20:19 +0500

    remove useless code for ci test

--- cliargstojson.go
+++ cliargstojson.go
@@ -1,16 +1,1 @@
 package cliargstojson
-
-import (
-	"strconv"
-
-	"github.com/sirupsen/logrus"
-)
-
-func Sum(a, b int) int {
-	if a < 0 || b < 0 {
-		aStr := strconv.Itoa(a)
-		bStr := strconv.Itoa(b)
-		logrus.Infof("a: %s, b: %s", aStr, bStr)
-	}
-	return a + b
-}
